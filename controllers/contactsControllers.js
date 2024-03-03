e3b205a97e4f98aa420a03ee8cc68293e02060df
UriyRatinskiy <uriy_mark@ukr.net>
2024-03-03 16:46:22 +0200

    hw2
    виправлення: додав result до res.status(200).json(result);

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -35,7 +35,7 @@ export const deleteContact = async (req, res, next) => {
     if (!result) {
       throw HttpError(404, `Not found`);
     }
-    res.status(200).json();
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
