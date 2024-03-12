072daa3daa26794e911ef15d0872b6b7c69c40c7
UriyRatinskiy <uriy_mark@ukr.net>
2024-03-12 23:26:54 +0200

    HW-04

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -5,6 +5,7 @@ export const createContactSchema = Joi.object({
   email: Joi.string().required(),
   phone: Joi.string().required(),
   favorite: Joi.boolean(),
+  owner: Joi.string().required(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -12,6 +13,7 @@ export const updateContactSchema = Joi.object({
   email: Joi.string(),
   phone: Joi.string(),
   favorite: Joi.boolean(),
+  owner: Joi.string(),
 });
 
 export const updateFavoriteSchema = Joi.object({
