84bb3b6f0f385427ff68614c479721e0fb5b9ee8
UriyRatinskiy <uriy_mark@ukr.net>
2024-03-19 21:56:20 +0200

    HW04

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ import express from "express";
 import authController from "../controllers/authController.js";
 
 import validateBody from "../decorators/validateBody.js";
+import aunthenticate from "../middlevares/aunthenticate.js";
 
 import { userSignupSchema, userSigninSchema } from "../schemas/userSchemas.js";
 
@@ -14,5 +15,9 @@ authRouter.post(
   authController.register
 );
 authRouter.post("/login", validateBody(userSigninSchema), authController.login);
+
+authRouter.post("/logout", aunthenticate, authController.logoutUser);
+
+authRouter.get("/current", aunthenticate, authController.getCurrentUser);
 
 export default authRouter;
