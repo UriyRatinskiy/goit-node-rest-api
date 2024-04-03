ac7b576197ed95027bb844dc6201476b8416e709
UriyRatinskiy <uriy_mark@ukr.net>
2024-04-03 21:16:41 +0300

    HW06 fix

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -5,7 +5,7 @@ const { UKR_NET_PASSWORD, UKR_NET_FROM } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.ukr.net",
-  port: 587,
+  port: 465,
   secure: true,
   auth: {
     user: UKR_NET_FROM,
